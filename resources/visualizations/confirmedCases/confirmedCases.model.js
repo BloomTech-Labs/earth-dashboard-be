ad723a3c074735ce3a562a1c4305699c64e9451f
Westley Feller <feller.westley@gmail.com>
2020-04-19 16:54:09 -0700

    DB queries for visualization data
    Aggregating data into arrays at DB level with raw queries

--- resources/visualizations/confirmedCases/confirmedCases.model.js
+++ resources/visualizations/confirmedCases/confirmedCases.model.js
@@ -1,5 +1,20 @@
 const db = require("../../../data/dbConfig");
 
-const getVisualizationData = () => db("USA Covid Daily Counts");
+const queryMapData = () =>
+  db
+    .raw(
+      'SELECT ARRAY_AGG("Lat") as lat, ARRAY_AGG("Lon") as lon, ARRAY_AGG("Cases") as z ' +
+        'FROM "USA Covid Daily Counts"'
+    )
+    .then((queryResult) => queryResult.rows);
 
-module.exports = { getVisualizationData };
+const queryDataByDate = () =>
+  db
+    .raw(
+      'SELECT "Date" as date, ARRAY_AGG("Lat") as lat, ARRAY_AGG("Lon") as lon, ARRAY_AGG("Cases") as z ' +
+        'FROM "USA Covid Daily Counts" ' +
+        'GROUP BY "Date"'
+    )
+    .then((queryResult) => queryResult.rows);
+
+module.exports = { queryMapData, queryDataByDate };
