82b9d78b1818bd0265bddc9b5b8fc4c69a30769d
Stephen Freeman <stephen.freeman.dev@gmail.com>
2020-05-01 17:13:10 -0700

    Added Route for Confirmed cases by country

--- resources/visualizations/cases/cases.router.js
+++ resources/visualizations/cases/cases.router.js
@@ -3,4 +3,7 @@ const controllers = require("./cases.controllers");
 
 router.route("/").get(controllers.getVisualizationData);
 
+// Get endpoint Data for confirmed cases by country for Racing Chart Visualization
+router.route("/confirmed").get(controllers.getConfirmedCasesData);
+
 module.exports = router;
