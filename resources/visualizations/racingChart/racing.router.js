1286691fae2370189838eef38962fd3fe4081f99
Stephen Freeman <stephen.freeman.dev@gmail.com>
2020-05-13 16:02:01 -0700

    Update resources/visualizations/racingChart/racing.router.js
    Co-authored-by: Westley Feller <feller.westley@gmail.com>

--- resources/visualizations/racingChart/racing.router.js
+++ resources/visualizations/racingChart/racing.router.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const controllers = require("./racing.controllers");
 
 // Get endpoint Data for the top country for Racing Chart Visualization
-router.route("/deaths").get(controllers.getConfirmedCasesData);
+router.route("/").get(controllers.getConfirmedCasesData);
 
 module.exports = router;
