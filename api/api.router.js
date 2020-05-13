9088d41ae06d816df397489aadb08130419b2eea
Westley Feller <feller.westley@gmail.com>
2020-05-12 22:33:47 -0700

    Air quality visualization data

--- api/api.router.js
+++ api/api.router.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const casesRouter = require("../resources/visualizations/cases/cases.router");
 const bubblesRouter = require("../resources/visualizations/bubbles/bubbles.router");
+const airRouter = require("../resources/visualizations/air/air.router");
 const controllers = require("./api.controllers");
 
 // Data needed for 'USA Covid-19 Confirmed Cases Daily Count' will be handled by the casesRouter and is accessed through /api/cases
 router.use("/cases", casesRouter);
 router.use("/bubbles", bubblesRouter);
+router.use("/air", airRouter);
 
 // /api routes to the api controller and sends back a basic status message to indicate API is up
 router.route("/").get(controllers.apiRoot);
