8ddfb248f5cd3d3b7a616c804bbe9d701d1f371b
Stephen Freeman <stephen.freeman.dev@gmail.com>
2020-05-13 17:54:26 -0700

    Made Changes after westley review

--- api/api.router.js
+++ api/api.router.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
 const casesRouter = require("../resources/visualizations/cases/cases.router");
 const bubblesRouter = require("../resources/visualizations/bubbles/bubbles.router");
-const controllers = require("./api.controllers");
+const racingRouter = require("../resources/visualizations/racingChart/racing.router");
 
 // Data needed for 'USA Covid-19 Confirmed Cases Daily Count' will be handled by the casesRouter and is accessed through /api/cases
 router.use("/cases", casesRouter);
 router.use("/bubbles", bubblesRouter);
-
 // /api routes to the api controller and sends back a basic status message to indicate API is up
-router.route("/deaths").get(controllers.apiRoot);
+router.use("/deaths", racingRouter);
 
 module.exports = router;
